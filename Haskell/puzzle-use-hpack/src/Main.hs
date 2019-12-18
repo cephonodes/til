3c2ae816baaa22f9ff9d15af9c2a3f02a4110b7f
cephonodes <yuki_cephonodes@outlook.com>
2019-12-18 22:31:31 +0900

    Haskellで「脳を鍛える数学パズル」 Q02 計算すべきすべてのパターンを列挙できるようになった

--- Haskell/puzzle-use-hpack/src/Main.hs
+++ Haskell/puzzle-use-hpack/src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import Language.Haskell.Interpreter
 import Data.List
+import Control.Monad
 
 main :: IO ()
 main = do
@@ -14,4 +15,13 @@ test1 xs ys = (++) (take 1 xs) (head ys) ++ (test1 (tail xs) (tail ys))
 test2 x = runInterpreter $ setImports ["Prelude"] >> eval x
 
 test3 = map show [1000..9999]
-test4 = ["+", "-", "*", "/", ""]  -- この中から3つを選んで順列を作るのを全パターンについてやりたい
+test4 = ["+", "-", "*", "/", ""]
+test5 = replicateM 3 test4
+
+test6 :: [Char] -> [[[Char]]] -> [[Char]]
+test6 x [] = []
+test6 x ys = test1 x (head ys) : (test6 x (tail ys))
+
+test7 :: [[Char]] -> [[[Char]]] -> [[[Char]]]
+test7 [] ys = []
+test7 xs ys = test6 (head xs) ys : (test7 (tail xs) ys)
