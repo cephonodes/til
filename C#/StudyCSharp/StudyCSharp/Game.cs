0b9c1323fee7c6843a52a6d01b74912daa7f679d
cephonodes <yuki_cephonodes@outlook.com>
2020-08-23 22:03:15 +0900

    C#の変数から制御構文までの言語仕様を流し見して、三目並べを作り始めた

---
+++ C#/StudyCSharp/StudyCSharp/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    public class Game
+    {
+        private bool isXNext;
+        private char[,] cells = new char[3, 3];
+
+        public int PutSymbol(int row, int column)
+        {
+            if (cells[row, column] != '\0')
+            {
+                return (-1);
+            }
+            else
+            {
+                cells[row, column] = isXNext ? 'X' : 'O';
+                isXNext = !isXNext;
+                return (0);
+            }
+        }
+
+        public string GenerateBoardVisual()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("  0 1 2  \n");
+			builder.Append("  - - - \n");
+			builder.Append($"0|{cells[0, 0]}|{cells[0, 1]}|{cells[0, 2]}|\n");
+			builder.Append("  - - - \n");
+			builder.Append($"1|{cells[1, 0]}|{cells[1, 1]}|{cells[1, 2]}|\n");
+			builder.Append("  - - - \n");
+			builder.Append($"2|{cells[2, 0]}|{cells[2, 1]}|{cells[2, 2]}|\n");
+			builder.Append("  - - - \n");
+            return (builder.ToString());
+        }
+    }
+}
---
+++ C#/StudyCSharp/StudyCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudyCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var game = new Game();
+
+			// まずは空っぽの盤面を表示する
+			Console.WriteLine(game.GenerateBoardVisual());
+
+			// 盤面に交互に記号を置かせる
+			for (int i=0; i<3; i++)
+			{
+				Console.WriteLine("Input position (Row): ");
+				var row = int.Parse(Console.ReadLine());
+				Console.WriteLine("Input position (Column): ");
+				var column = int.Parse(Console.ReadLine());
+
+				if (game.PutSymbol(row, column) != 0)
+				{
+					Console.WriteLine("Error");
+				}
+
+				Console.WriteLine(game.GenerateBoardVisual());
+			}
+		}
+	}
+}
